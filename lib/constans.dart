25214432698f778960cccdb02fc45563bda2662b
NOUR WALEED <nourwaleed1020@gmail.com>
2026-06-09 00:00:36 +0300

    refactor init state & navigate to home view

--- lib/constans.dart
+++ lib/constans.dart
@@ -1,3 +1,4 @@
 import 'package:flutter/widgets.dart';
 
 const kPprimaryColor = Color(0xff100B20);
+const kDuration = Duration(microseconds: 250);
--- lib/features/splash/presentation/views/widget/splash_view_body.dart
+++ lib/features/splash/presentation/views/widget/splash_view_body.dart
@@ -1,6 +1,10 @@
+import 'package:bookly_app/constans.dart';
 import 'package:bookly_app/core/utils/assets.dart';
+import 'package:bookly_app/features/home/presentation/views/home_view.dart';
 import 'package:bookly_app/features/splash/presentation/views/widget/sliding_text.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get/utils.dart';
 
 class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
@@ -18,17 +22,8 @@ class _SplashViewBodyState extends State<SplashViewBody>
   void initState() {
     super.initState();
 
-    animationController = AnimationController(
-      vsync: this,
-      duration: const Duration(seconds: 1),
-    );
-
-    slideAnimation = Tween<Offset>(
-      begin: const Offset(0, 5),
-      end: Offset.zero,
-    ).animate(animationController);
-
-    animationController.forward();
+    initSlidingAnimation();
+    navigateToHomeView();
   }
 
   @override
@@ -50,5 +45,29 @@ class _SplashViewBodyState extends State<SplashViewBody>
         ],
       ),
     );
+  }
+
+  void initSlidingAnimation() {
+    animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 1),
+    );
+
+    slideAnimation = Tween<Offset>(
+      begin: const Offset(0, 5),
+      end: Offset.zero,
+    ).animate(animationController);
+
+    animationController.forward();
+  }
+
+  void navigateToHomeView() {
+    Future.delayed(const Duration(seconds: 2), () {
+      Get.to(
+        () => const HomeView(),
+        transition: Transition.fade,
+        duration: kDuration,
+      );
+    });
   }
 }
