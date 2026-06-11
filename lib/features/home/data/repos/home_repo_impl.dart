2f91b4c84f82b63eb59bc2a0e1a64ff4882ad90e
NOUR WALEED <nourwaleed1020@gmail.com>
2026-06-11 13:14:18 +0300

    implement fetch  newest books

--- lib/features/home/data/repos/home_repo_impl.dart
+++ lib/features/home/data/repos/home_repo_impl.dart
@@ -1,13 +1,30 @@
 import 'package:bookly_app/core/errors/failure.dart';
+import 'package:bookly_app/core/utils/api_services.dart';
 import 'package:bookly_app/features/home/data/model/book_model/book_model.dart';
 import 'package:bookly_app/features/home/data/repos/home_repo.dart';
 import 'package:dartz/dartz.dart';
 
 class HomeRepoImpl implements HomeRepo {
+  final ApiServices apiServices;
+
+  HomeRepoImpl({required this.apiServices});
   @override
-  Future<Either<Failure, List<BookModel>>> fetchBestSellerBooks() {
-    // TODO: implement fetchBestSellerBooks
-    throw UnimplementedError();
+  Future<Either<Failure, List<BookModel>>> fetchNewestBooks() async {
+    try {
+      var data = await apiServices.get(
+        endPoints:
+            'volumes?filter=free-ebooks&orderBy=newest&q=programming&key=AIzaSyBVYceBw9s0XoOuUFUtjsKfbRfg4yueEMQ',
+      );
+
+      List<BookModel> books = [];
+
+      for (var item in data['items']) {
+        books.add(BookModel.fromJson(item));
+      }
+      return right(books);
+    } catch (e) {
+      return left(ServerEror());
+    }
   }
 
   @override
