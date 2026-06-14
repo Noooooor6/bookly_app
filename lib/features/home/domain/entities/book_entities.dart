52921bfd7dc8502be7f610bab131e6c017a66e40
NOUR WALEED <nourwaleed1020@gmail.com>
2026-06-14 21:42:46 +0300

    book entities done

---
+++ lib/features/home/domain/entities/book_entities.dart
@@ -0,0 +1,13 @@
+class BookEntities {
+  final String image;
+  final String title;
+  final String auther;
+  final num price;
+
+  BookEntities({
+    required this.image,
+    required this.title,
+    required this.auther,
+    required this.price,
+  });
+}
