d250363c62366bdd031aee93ca4cd9c6b2a008b1
NOUR WALEED <nourwaleed1020@gmail.com>
2026-06-15 01:34:03 +0300

    use cases done

---
+++ lib/features/home/domain/use_cases/fetch_newest_book_use_case.dart
@@ -0,0 +1,17 @@
+import 'package:bookly_app/core/errors/failure.dart';
+import 'package:bookly_app/core/use_cases/use_case.dart';
+import 'package:bookly_app/features/home/data/model/book_model/book_model.dart';
+import 'package:bookly_app/features/home/domain/repos/home_repo.dart';
+import 'package:dartz/dartz.dart';
+
+class FetchNewestBooksUseCase extends UseCase<List<BookModel>, int> {
+  final HomeRepo homeRepo;
+
+  FetchNewestBooksUseCase({required this.homeRepo});
+
+  @override
+  Future<Either<Failure, List<BookModel>>> call([int? param]) {
+    // TODO: implement call
+    throw UnimplementedError();
+  }
+}
