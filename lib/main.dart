0c39dd34aed038e04f8328bfc594f9f0dfcf52e1
NOUR WALEED <nourwaleed1020@gmail.com>
2026-06-15 16:16:48 +0300

    register adabter and open box

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,21 @@
 import 'package:bookly_app/constans.dart';
-import 'package:bookly_app/core/utils/api_services.dart';
 import 'package:bookly_app/core/utils/app_router.dart';
 import 'package:bookly_app/core/utils/services_locator.dart';
-import 'package:bookly_app/features/home/data/repos/home_repo_impl.dart';
+import 'package:bookly_app/features/home/domain/entities/book_entities.dart';
+import 'package:bookly_app/features/home/domain/repos/home_repo_impl.dart';
 import 'package:bookly_app/features/home/presentation/manager/featured_books_cubit/featured_books_cubit.dart';
 import 'package:bookly_app/features/home/presentation/manager/newest_books_cubit/newest_books_cubit.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:hive_flutter/adapters.dart';
 
-void main() {
+void main() async {
   SetUpServicesLocator();
   runApp(const MyApp());
+
+  Hive.registerAdapter(BookEntitiesAdapter());
+  await Hive.openBox(kFeaturedBox);
 }
 
 class MyApp extends StatelessWidget {
@@ -29,8 +33,7 @@ class MyApp extends StatelessWidget {
         ),
         BlocProvider(
           create: (context) =>
-              NewestBooksCubit(getIt.get<HomeRepoImpl>())
-                ..fetchNewestBooks(),
+              NewestBooksCubit(getIt.get<HomeRepoImpl>())..fetchNewestBooks(),
         ),
       ],
       child: MaterialApp.router(
