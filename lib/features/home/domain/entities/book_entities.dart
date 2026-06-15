3943911689136dca95a9848b0826c2ff788a29c2
NOUR WALEED <nourwaleed1020@gmail.com>
2026-06-15 16:10:10 +0300

    generate book entites adapter

--- lib/features/home/domain/entities/book_entities.dart
+++ lib/features/home/domain/entities/book_entities.dart
@@ -1,10 +1,21 @@
+import 'package:hive/hive.dart';
+part 'book_entities.g.dart';
+
+@HiveType(typeId: 0)
 class BookEntities {
-  final String image;
+  @HiveField(0)
+  final String bookId;
+  @HiveField(1)
+  final String? image;
+  @HiveField(2)
   final String title;
-  final String auther;
-  final num price;
+  @HiveField(3)
+  final String? auther;
+  @HiveField(4)
+  final num? price;
 
   BookEntities({
+    required this.bookId,
     required this.image,
     required this.title,
     required this.auther,
